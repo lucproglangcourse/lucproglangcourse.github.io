75250d8045a4e5f07fdf919de5e732592224b18c
Konstantin Läufer <klaeufer@gmail.com>
2021-08-25 11:52:26 -0500

    improved deploy script
    Signed-off-by: Konstantin Läufer <klaeufer@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -13,5 +13,5 @@ git log main --pretty=short --abbrev-commit > $DEST/COMMITS.txt
 
 pushd $DEST
 git add -A
-git commit -m "Generated site for `git log main -1 --pretty=oneline --abbrev-commit`" && git push
+git commit -m "Generated site for $(git log main -1 --pretty=oneline --abbrev-commit)" && git push
 popd
