621ca899d6881ebdd03a8c388dd0633b6edc3186
Konstantin Läufer <klaeufer@gmail.com>
2021-08-24 19:52:52 -0500

    updated deploy script
    Signed-off-by: Konstantin Läufer <klaeufer@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -9,9 +9,9 @@ cp -a build/html/* $DEST/
 cp build/latex/*.pdf $DEST/download/
 cp build/epub/*.epub $DEST/download/proglang.epub
 
-git log master --pretty=short --abbrev-commit > $DEST/COMMITS.txt
+git log main --pretty=short --abbrev-commit > $DEST/COMMITS.txt
 
 pushd $DEST
 git add -A
-git commit -m "Generated site for `git log master -1 --pretty=short --abbrev-commit`" && git push
+git commit -m "Generated site for `git log main -1 --pretty=short --abbrev-commit`" && git push
 popd
