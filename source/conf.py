71e122bc1b08746e6453ecc6a01ceb816081e073
Konstantin Läufer <laufer@cs.luc.edu>
2022-05-16 22:32:48 -0500

    updated copyright years
    Signed-off-by: Konstantin Läufer <laufer@cs.luc.edu>

--- source/conf.py
+++ source/conf.py
@@ -52,7 +52,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Loyola University Chicago COMP 371/471 Lecture Notes'
-copyright = u'2017-2019, Konstantin Läufer'
+copyright = u'2017-2022, Konstantin Läufer'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
